513f7a03fac94f4ec9c02e054dd0a3169bf8d702
Lee Fogg <leefogg@users.noreply.github.com>
2021-03-27 15:35:25 +0000

    id -> gl_GlobalInvocationID.x

--- OpenGL-Compute-Example/update.glsl
+++ OpenGL-Compute-Example/update.glsl
@@ -18,25 +18,26 @@ layout(local_size_x = 1024, local_size_y = 1, local_size_z = 1) in;
 const float G = 0.0001;
 
 void main() {
-  // Get easier references to positions/velocities
-  vec4 position = positions[gl_GlobalInvocationID.x];
-  vec4 velocity = velocities[gl_GlobalInvocationID.x];
+	// Get easier references to positions/velocities
+	uint id = gl_GlobalInvocationID.x;
+	vec4 position = positions[id];
+	vec4 velocity = velocities[id];
 
-  //Build our 2D acceleration using universal gravitation
-  vec2 r;
-  r.x = cursor.x - position.x;
-  r.y = cursor.y - position.y;
-  float magnitude = length(r);
-  r = normalize(r);
-  vec2 accel2D = r * (G) / (magnitude * magnitude);
+	//Build our 2D acceleration using universal gravitation
+	vec2 r;
+	r.x = cursor.x - position.x;
+	r.y = cursor.y - position.y;
+	float magnitude = length(r);
+	r = normalize(r);
+	vec2 accel2D = r * (G) / (magnitude * magnitude);
 
-  //Update our velocity and position
-  velocity.x += accel2D.x;
-  velocity.y += accel2D.y;
+	//Update our velocity and position
+	velocity.x += accel2D.x;
+	velocity.y += accel2D.y;
 
-  position = position + velocity * deltaTime;
+	position = position + velocity * deltaTime;
 
-  //Save our position and velocities for displaying
-  positions[gl_GlobalInvocationID.x] = position;
-  velocities[gl_GlobalInvocationID.x] = velocity;
+	//Save our position and velocities for displaying
+	positions[id] = position;
+	velocities[id] = velocity;
 }
